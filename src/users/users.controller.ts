358b1a40fa0279e745d35afb5b9ff7f66ecdfc70
NicholasWM <nicholaswesleymazzei@gmail.com>
2021-03-09 13:52:21 -0300

    refact: Remove inactive imports

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,14 +2,8 @@ import { Controller, Post, Body, ValidationPipe, UseGuards, Get, Param, UseInter
 import {UsersService} from './users.service'
 import {CreateUserDTO} from './dtos/create-user.dto'
 import {ReturnUserDTO} from './dtos/return-user.dto'
-import { AuthGuard } from '@nestjs/passport';
-import { RolesGuard } from 'src/auth/roles.guards';
 import { ApiTags } from '@nestjs/swagger';
 import { User } from './user.entity';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import { editFileName, imageFileFilter } from 'src/utils/file-upload';
-import { GetUser } from 'src/auth/get-user.decorator';
 
 @ApiTags('Users')
 @Controller('users')
